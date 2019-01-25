7a4c468bda7c4c14c82bce9d845ba9fd147ac4c2
Dadao <wangdy9217@163.com>
2019-01-25 16:34:28 +0800

    Update DataUpdate.R

--- DataUpdate.R
+++ DataUpdate.R
@@ -3,12 +3,18 @@ load("./hstr.RData")
 
 DataUpdate <- function(data_source_path=FALSE,newdata_path=FALSE,dwnld_data = FALSE
                        ,save_rdata = FALSE){
-  #数据更新部分---start。
-  print("dataupdate start!")
+  
+  #data_source_path=FALSE
+  #newdata_path="F:/RWD/fengzhuang/NO12_1219-zhuliushijianwrong-total-added.csv"
+  #dwnld_data = FALSE
+  #save_rdata = FALSE
+  ##数据更新部分---start。
+  print(paste0("dataupdate start! >>>",Sys.time()))
   if(newdata_path != FALSE & (exists("data_1022")) ){
+    
     newdata <- read.csv(newdata_path,header = T,fileEncoding = 'GBK')
     data_1022 <- rbind(data_1022,newdata)
-    data_1022 <- data_1022[duplicated(data_1022),]#避免重复update造成的数据重复。
+    data_1022 <- data_1022[!duplicated(data_1022),]#避免重复update造成的数据重复。
     if(dwnld_data != FALSE){#最新数据下载到本地。
       filename <- paste0("data_updated_",substring(Sys.time(),1,10),".csv")
       write.csv(data_1022,filename)
@@ -25,7 +31,7 @@ DataUpdate <- function(data_source_path=FALSE,newdata_path=FALSE,dwnld_data = FA
     }
   }
   assign("data_1022",data_1022,envir=.GlobalEnv)#以备复用。
-  print("dataupdate finish!")
+  print(paste0("dataupdate finish!>>>",Sys.time()))
   #return(list(data_1022 = data_1022))
   #数据更新部分---end。
   if(save_rdata != FALSE){
@@ -33,3 +39,4 @@ DataUpdate <- function(data_source_path=FALSE,newdata_path=FALSE,dwnld_data = FA
     print(file.mtime("./hstr.RData"))
   }
 }
+
